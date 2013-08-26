90399d58f3fff784023173f687b0763e597560ae
hanmomhanda <hanmomhanda@gmail.com>
2013-08-26 14:07:12 +0900

    Initial SetUp for fresto-ui

---
+++ web/lib/APMJsUtil.js
@@ -0,0 +1,9 @@
+
+	function randomString() {
+		return Math.random().toString(36).slice(2);
+	}
+
+	function randomColorString() {
+		return "#"+Math.floor(Math.random()*16777215).toString(16);
+	}
+
